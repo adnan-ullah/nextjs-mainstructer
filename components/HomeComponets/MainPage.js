953b42812f345497e365b073ef0b701cb4853fde
adnan-ullah <daaztanbd71@gmail.com>
2022-07-19 00:40:22 +0600

    Fix bad repo

--- components/HomeComponets/MainPage.js
+++ components/HomeComponets/MainPage.js
@@ -4,14 +4,28 @@ export default function MainPage() {
   return (
     <div className="randomBox h-full w-full text-green-300">
       <div className="grid grid-rows-3  grid-flow-col gap-4">
-        <div className="bg-white h-128">
+       
+        <div className="h-128">
           <div className="grid h-128 grid-cols-3">
-            <div className="bg-slate-300 opacity-50 col-span-2"></div>
+            <div className="bg-slate-300 opacity-90 col-span-2"></div>
             <div className="bg-white"></div>
           </div>
         </div>
-        <div className=" bg-green-300 h-128 ">02</div>
-        <div className="bg-blue-300 h-128  ">03</div>
+
+
+        <div className="h-128">
+          <div className="grid h-128 grid-cols-3">
+            <div className="bg-red-900 opacity-90 col-span-2"></div>
+            <div className="bg-white"></div>
+          </div>
+        </div>
+
+        <div className="h-128">
+          <div className="grid h-128 grid-cols-3">
+            <div className="bg-yellow-300 opacity-90 col-span-2"></div>
+            <div className="bg-white"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
